9bc00d7323b3a5a0c3df1b890496cb083a9f402b
arianjafari <ajafari@iastate.edu>
2022-02-05 20:00:30 -0500

    inti

--- train.py
+++ train.py
@@ -17,3 +17,79 @@ import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 from torchvision.utils import make_grid
 from torchsummary import summary
+
+from model import VectorQuantizerEMA, Encoder, Decoder, VQVAE
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+training_data = datasets.CIFAR10(root="data", train=True, download=True,
+                                  transform=transforms.Compose([
+                                      transforms.ToTensor(),
+                                      transforms.Normalize((0.5,0.5,0.5), (1.0,1.0,1.0))
+                                  ]))
+
+validation_data = datasets.CIFAR10(root="data", train=False, download=True,
+                                  transform=transforms.Compose([
+                                      transforms.ToTensor(),
+                                      transforms.Normalize((0.5,0.5,0.5), (1.0,1.0,1.0))
+                                  ]))
+
+data_variance = np.var(training_data.data / 255.0)
+print("data_variance: ", data_variance)  
+
+batch_size = 256
+num_training_updates = 60000
+
+num_hiddens = 128
+num_residual_hiddens = 32
+num_residual_layers = 2
+
+embedding_dim = 64
+num_embeddings = 512
+
+commitment_cost = 0.25
+
+decay = 0.99
+
+learning_rate = 1e-4
+
+training_loader = DataLoader(training_data, 
+                             batch_size=batch_size, 
+                             shuffle=True,
+                             pin_memory=True)
+
+validation_loader = DataLoader(validation_data,
+                               batch_size=32,
+                               shuffle=True,
+                               pin_memory=True)
+
+model = VQVAE().to(device)
+optimizer = optim.Adam(model.parameters(), lr=learning_rate, amsgrad=False)
+
+model.train()
+train_res_recon_error = []
+train_res_perplexity = []
+
+for i in range(num_training_updates):
+    (data, _) = next(iter(training_loader))
+    data = data.to(device)
+    optimizer.zero_grad()
+    
+    data_recon, vq_loss = model(data)
+    recon_error = F.mse_loss(data_recon, data) / data_variance
+    loss = recon_error + vq_loss
+    loss.backward()
+    
+    optimizer.step()
+    
+    train_res_recon_error.append(recon_error.item())
+#     train_res_perplexity.append(perplexity.item())
+    
+    if (i+1) % 2000 == 0:
+        print('%d iterations' % (i+1))
+        print('recon_error: %.3f' % np.mean(train_res_recon_error[-100:]))
+#         print('perplexity: %.3f' % np.mean(train_res_perplexity[-100:]))
+        print()
+
+train_res_recon_error_smooth = savgol_filter(train_res_recon_error, 201, 7)
+# train_res_perplexity_smooth = savgol_filter(train_res_perplexity, 201, 7)
